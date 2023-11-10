77483dc948e13633b65a6f861e3faa5a578caf7f
MCprotein <chss3339@gmail.com>
2023-11-10 21:56:25 +0900

    feat: 테스트 파일 병합 piping

--- src/piping-pattern/generate-hashed.js
+++ src/piping-pattern/generate-hashed.js
@@ -2,7 +2,7 @@ import { createReadStream, createWriteStream } from 'fs'
 import { createHash } from 'crypto'
 
 /**
- * pipe()를 호출할 때 {end: false}를 옵션으로 안하면 md5Stream, sha1Stream은 inputStream이 종료될때
+ * pipe()를 호출할 때 { end: false }를 옵션으로 안하면 md5Stream, sha1Stream은 inputStream이 종료될때
  * 자동으로 종료된다.
  * 분기된 스트림은 동일한 청크를 수신한다.
  * 배압(Backpressure)은 바로 발생한다. InputStream에서 오는 흐름은 매우 빠르지만 분기된 스트림은 느리다.
